5495ba656dc3b1f19082560e5a4c745c3831e85f
Guilherme Abreu <Guilherme.abreu2019@gmail.com>
2025-06-03 16:52:17 -0300

    Feature/export new query (#45)
    * Adiciona novas exportações para QueryBuildSQlite e QueryDDL no arquivo index.ts, melhorando a modularidade e a organização do código.
    
    * 3.1.1

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { DatabaseConnectionOrmSQlite } from './database/database';
 import { QueryBuildOrmSQlite } from './query/query-build';
+import { QueryBuildSQlite } from './query/query-build-sqlite';
+import { QueryDDL } from './query/query-ddl';
 
 
 export * from './decoratiors/decoratiors.orm';
 export * from './definitions';
-export { DatabaseConnectionOrmSQlite, QueryBuildOrmSQlite };
+export { DatabaseConnectionOrmSQlite, QueryBuildOrmSQlite, QueryBuildSQlite, QueryDDL };
 
